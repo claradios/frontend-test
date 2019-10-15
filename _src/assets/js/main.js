655028d165f3377196423226b3c0cb5fac6b7148
claradios <clara.dios.diez@gmail.com>
2019-10-15 16:34:35 +0200

    call Api working with fetch

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,13 +1,21 @@
 'use strict';
 
-const input = document.querySelector ('.main__input-search');
+const input = document.querySelector('.main__input-search');
 const btn = document.querySelector('.main__btn-search');
 const container = document.querySelector('.main__user_container');
 
-function showResults(){
-  console.log(input,container);
+function callApi() {
+  fetch(`https://api.github.com/users/${input.value}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    });
 }
 
-btn.addEventListener('click',showResults);
+function showResults() {
+  callApi();
+}
+
+btn.addEventListener('click', showResults);
 
 
