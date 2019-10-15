7f47e11ce93e63260a42ea7e670d436a1c92936b
claradios <clara.dios.diez@gmail.com>
2019-10-15 16:26:26 +0200

    define main structure with listener

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,7 +1,13 @@
 'use strict';
 
-
 const input = document.querySelector ('.main__input-search');
 const btn = document.querySelector('.main__btn-search');
+const container = document.querySelector('.main__user_container');
+
+function showResults(){
+  console.log(input,container);
+}
+
+btn.addEventListener('click',showResults);
 
 
