8a946cff30c102071bc964e4269598ce1104f008
claradios <clara.dios.diez@gmail.com>
2019-10-15 16:56:42 +0200

    get name bio username and img from user

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -2,18 +2,45 @@
 
 const input = document.querySelector('.main__input-search');
 const btn = document.querySelector('.main__btn-search');
-const container = document.querySelector('.main__user_container');
+const userContainer = document.querySelector('.main__user_container');
 
-function callApi() {
+
+
+
+// function callApi() {
+//   fetch(`https://api.github.com/users/${input.value}`)
+//     .then(response => response.json())
+//     .then(data => {
+//       console.log(data);
+//     });
+// }
+
+function showResults() {
+
   fetch(`https://api.github.com/users/${input.value}`)
     .then(response => response.json())
     .then(data => {
-      console.log(data);
-    });
-}
+      const newSpan = document.createElement('span');
+      const newTitle = document.createElement('h2');
+      const newImg = document.createElement('img');
+      const newText = document.createElement('p');
 
-function showResults() {
-  callApi();
+      const userName = document.createTextNode(`@${data.login}`);
+      const name = document.createTextNode(data.name);
+      const bio = document.createTextNode(data.bio);
+
+      newImg.src = data.avatar_url;
+
+      newSpan.appendChild(userName);
+      newTitle.appendChild(name);
+      newText.appendChild(bio);
+
+      userContainer.appendChild(newTitle);
+      userContainer.appendChild(newText);
+      userContainer.appendChild(newImg);
+
+    });
+
 }
 
 btn.addEventListener('click', showResults);
